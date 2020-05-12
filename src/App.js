dd220541bc05f722ee37086594b31f87c95b76f9
Smith <kory.smith@kroger.com>
2020-05-12 14:45:14 -0400

    Show how JSX is rendered by React.createElement

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 class App extends Component {
   // This is a method, remember.
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App!</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App!</h1>
+    //   </div>
+    // );
+    // ^ The above and below are the same
+    return React.createElement('div', {className: "App"}, React.createElement('h1', null, "Does this work now?"))
   }
 }
 
