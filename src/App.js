3dcc5584a589bf6afc543f6fe8faffba39cf6a63
Smith <kory.smith@kroger.com>
2020-05-12 14:44:18 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  // This is a method, remember.
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
