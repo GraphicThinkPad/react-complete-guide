73c44fde72b5dea485e14d27e4ab804a67111da1
Smith <kory.smith@kroger.com>
2020-05-13 13:38:54 -0400

    Make Person compenent use props

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Kory" age="25" />
+        <Person name ="Sydney" age="25">My Hobbies: Acting</Person>
+        <Person name="Max" age="28" />
       </div>
     );
     // ^ The above and below are the same
