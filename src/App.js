9d0e89865b518d3b26464ae787e9cfce07f3a8e6
Smith <kory.smith@kroger.com>
2020-05-14 13:28:21 -0400

    Add a handler to the button

--- src/App.js
+++ src/App.js
@@ -5,27 +5,22 @@ import Person from "./Person/Person.js";
 class App extends Component {
   state = {
     persons: [
-      {
-        name: "Max",
-        age: 28,
-      },
-      {
-        name: "Kory",
-        age: 25,
-      },
-      {
-        name: "Sydney",
-        age: 25,
-      },
+      { name: "Max", age: 28, },
+      { name: "Kory", age: 25, },
+      { name: "Sydney", age: 25, },
     ],
   };
+
+switchNameHandler = () => {
+  console.log("Whee");
+}
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
           My Hobbies: Acting
