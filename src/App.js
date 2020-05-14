af36c0d46aca7a870816d4a107bb96d7fa962b6c
Smith <kory.smith@kroger.com>
2020-05-14 13:40:45 -0400

    Use event listener to change state and trigger DOM rerender

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import Person from "./Person/Person.js";
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 28, },
-      { name: "Kory", age: 25, },
-      { name: "Sydney", age: 25, },
+      { name: "Max", age: 28 },
+      { name: "Kory", age: 25 },
+      { name: "Sydney", age: 25 },
     ],
+    otherState: "Some other value",
   };
 
-switchNameHandler = () => {
-  console.log("Whee");
-}
+  switchNameHandler = () => {
+    // console.log("Whee");
+    // Don't do this: this.state.persons[0].name = "Maximilian"
+    this.setState({
+      persons: [
+        { name: "Maxxy", age: 28 },
+        { name: "Kory", age: 25 },
+        { name: "Sydney", age: 26 },
+      ],
+    });
+  };
 
   render() {
     return (
@@ -21,11 +30,20 @@ switchNameHandler = () => {
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
           My Hobbies: Acting
         </Person>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
       </div>
     );
     // ^ The above and below are the same
