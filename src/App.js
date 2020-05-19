8cdca8a153ac8d2c459380d694071b616b00789b
Smith <kory.smith@kroger.com>
2020-05-19 16:37:00 -0400

    Add local styling to the button

--- src/App.js
+++ src/App.js
@@ -32,18 +32,29 @@ class App extends Component {
         { name: "Sydney", age: 26 },
       ],
     });
-  }
+  };
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler("Maximint")}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler("Maximint")}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[2].name}
