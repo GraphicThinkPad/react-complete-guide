52d1d98420f61cfeb12cc25c2faa725a686e0286
Smith <kory.smith@kroger.com>
2020-06-22 15:05:47 -0400

    Add handler for changed name to person 2

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[2].name}
