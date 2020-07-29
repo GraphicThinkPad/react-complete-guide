7402f80d3bc521c7db9a567dc667a1589fdabe5f
Kory Smith <kory.smith@kroger.com>
2020-07-29 13:20:47 -0400

    allow withClass to pass whatever props it receives to its wrapped component.

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import styles from "./Person.css";
 import Aux from "../../../hoc/Aux"
-import WithClass from "../../../hoc/withClass"
+import withClass from "../../../hoc/withClass"
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
       // <div className={styles.Person}>
-      <WithClass classes={styles.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I am {this.props.name} and I am {this.props.age} years old
         </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
        {/* </div> */}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default Person;
+export default withClass(Person, styles.Person);
